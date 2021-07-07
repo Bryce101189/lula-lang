54dc64c6c76a22779ff4fd6a472ef80fd5323a78
Suirabu <42366259+Bryce101189@users.noreply.github.com>
2021-07-07 01:33:55 -0700

    Add file reading and basic interface
    This commit adds a basic interface for which a user can choose a
    file to be run as a script. This file will then be opened and have
    its contents read into a String to be split into tokens in a later
    commit.

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::{env, fs::File, io::Read};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // Check for input file
+    if args.len() < 2 {
+        eprintln!("Fatal error: No input file provided");
+        return;
+    }
+
+    let in_file_path = args.get(1).unwrap();
+
+    // Check if input file uses the '.lla' file extension
+    if !in_file_path.ends_with(".lla") {
+        eprintln!("Fatal error: Input file does not use the '.lla' file extension");
+        return;
+    }
+
+    // Attempt to open input file
+    let mut in_file = match File::open(in_file_path) {
+        Ok(file) => file,
+        Err(..) => {
+            eprintln!(
+                "Fatal error: Failed to open file '{}'\n~ are you sure this file exists?",
+                in_file_path
+            );
+            return;
+        }
+    };
+
+    // Read file contents into string
+    let mut in_file_contents = String::new();
+
+    if in_file.read_to_string(&mut in_file_contents).is_err() {
+        eprintln!("Fatal error: Failed to read file contents into string");
+        return;
+    }
+}
